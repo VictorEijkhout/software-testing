b19a8e4a68142453033d1a373a27b92cdf68051b
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-09-07 18:31:36 -0500

    compiler looping

--- hdf5/fmod_frontera_jail.sh
+++ hdf5/fmod_frontera_jail.sh
@@ -2,14 +2,20 @@
 
 module reset >/dev/null 2>&1
 echo "==== TACC modules"
-compiler=intel/19
-echo "---- computer $compiler"
-module load $compiler
-for v in 1.10.4 1.14.0 ; do
-    module load hdf5/$v >/dev/null 2>&1
-    ##v=$( modversion hdf5 )
-    echo "compile with hdf5 $v"
-    cmdline="ifort -c -I${TACC_HDF5_INC} fmod.F90"
-    echo " .. $cmdline"
-    eval $cmdline
+for compiler in intel/19 intel/23 gcc/9 ; do
+    echo "---- computer $compiler"
+    case $compiler in ( intel* ) ifort=ifort ;; ( gcc* ) ifort=gfortran ;; esac
+    module load $compiler >/dev/null 2>&1
+    for v in 1.10.4 1.14.0 ; do
+        module load hdf5/$v >/dev/null 2>&1
+        if [ $? -eq 0 ] ; then
+            echo "compile with hdf5 $v"
+            cmdline="$ifort -c -I${TACC_HDF5_INC} fmod.F90"
+            echo " .. $cmdline"
+            eval $cmdline
+        else
+            echo "could not load hdf5 $v"
+        fi
+    done
 done
+
