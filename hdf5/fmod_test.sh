1dda840180f62698466b0267539a976be79f19d8
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-09-07 08:08:18 -0500

    test script

--- hdf5/fmod_test.sh
+++ hdf5/fmod_test.sh
@@ -7,8 +7,18 @@ function modversion {
 }
 
 module reset >/dev/null 2>&1
+echo "==== TACC modules"
 module load intel/19
 for v in 1.10.4 1.14.0 ; do 
+    module load hdf5/$v >/dev/null 2>&1
+    ##v=$( modversion hdf5 )
+    echo "compile with hdf5 $v"
+    ifort -c -I${TACC_HDF5_INC} fmod.F90
+done
+
+echo "==== private modules"
+source ${HOME}/Software/env_fortran_intel19.sh
+for v in 1.14.0 ; do 
     module load hdf5/$v >/dev/null 2>&1
     ##v=$( modversion hdf5 )
     echo "compile with hdf5 $v"
