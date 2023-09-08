4ad05b16773a0840dbce6e2786bf7e98c8564ea8
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-09-08 08:23:36 -0500

    common make rules

--- petsc/mumpsi64.c
+++ petsc/mumpsi64.c
@@ -1,7 +1,10 @@
 #include "petsc.h"
 
 int main() {
+  PetscInitialize(0,0,0,0);
+  MPI_Comm comm = MPI_COMM_WORLD;
   KSP ksp;
+  KSPCreate(comm,&ksp);
   KSPSetType(ksp, KSPPREONLY);
   PC pc;
   KSPGetPC(ksp, &pc);
