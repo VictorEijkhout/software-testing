7db85ad02c34285a29b48264e6084211b1d824a4
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-09-08 15:45:34 -0500

    test bunch of frontera compilers

--- mpi/mpif08.sh
+++ mpi/mpif08.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-mpif09 -c mpif08
+module reset >/dev/null 2>&1
+echo "==== TACC modules"
+for compiler in gcc/9 intel/19 gcc13 intel/23 ; do 
+    module load $compiler >/dev/null 2>&1
+    echo "Trying compiler $compiler"
+    if [ $? -eq 0 ] ; then 
+	retcode=0 && mpif90 -c mpif08.F90 || retcode=$?
+	if [ $retcode -eq 0 ] ; then 
+	    echo " .. success"
+	else
+	    echo && echo "ERROR could not compile with $compiler" && echo
+	fi 
+    else
+	echo " .. could not load compiler $compiler"
+    fi
+done
+
