821725ddb49c0b468d0b7be24ac5a5af7eaaf292
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-10-04 10:18:27 -0500

    fftw single test

--- fftw3/local_test.sh
+++ fftw3/local_test.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
 
 package=fftw3
+version=3.3.10
 
 ##
 ## test all programs for this package,
 ## looping over locally available modules
 ##
 
-function failure() {
-    if [ $1 -gt 0 ] ; then 
-	echo && echo "ERROR failed $2" && echo 
-    fi
-}
-
-version=3.3.10
-function usage() {
-    echo "Usage: $0 [ -v version (default=${version} ]"
-}
-if [ $# -eq 1 -a "$1" = "-h" ] ; then
-    usage && exit 0
-fi
-while [ $# -gt 0 ] ; do
-    if [ "$1" = "-v" ] ; then
-	shift && version="$1" && shift
-    fi
-done
+source ../options.sh
 
 module reset >/dev/null 2>&1
 echo "================"
@@ -39,15 +23,7 @@ for compiler in intel/19 intel/23 gcc/9 gcc/13 ; do \
     module load ${package}/${version} >/dev/null 2>&1
     if [ $? -eq 0 ] ; then
 
-	echo "==== Test if we can compile"
-	retcode=0
-	../cmake_test.sh -p ${package} has.c >${compilelog} 2>&1 || retcode=$?
-	failure $retcode "basic compilation"
-
-	echo "==== Test if we can compile single"
-	retcode=0
-	../cmake_test.sh -p ${package} has.cf >${compilelog} 2>&1 || retcode=$?
-	failure $retcode "single precision compilation"
+	source fftw3_tests.sh
 
     else
 	echo "WARNING could not load ${package}/${version}"
