0c535c148d546417fded3611641d5b19ccd620de
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-10-04 08:49:20 -0500

    factor out common stuff

---
+++ setup.sh
@@ -0,0 +1,17 @@
+function failure() {
+    if [ $1 -gt 0 ] ; then 
+	echo && echo "ERROR failed $2" && echo 
+    fi
+}
+
+function usage() {
+    echo "Usage: $0 [ -v version (default=${version}) ]"
+}
+if [ $# -eq 1 -a "$1" = "-h" ] ; then
+    usage && exit 0
+fi
+while [ $# -gt 0 ] ; do
+    if [ "$1" = "-v" ] ; then
+	shift && version="$1" && shift
+    fi
+done
