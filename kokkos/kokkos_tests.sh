acccd690dd69c3e63d155958de53bb7d07174820
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-10-11 14:06:34 -0500

    tests for ls6

--- kokkos/kokkos_tests.sh
+++ kokkos/kokkos_tests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+module unload cuda
+module load kokkos/4.1.00
+g++ -I${TACC_KOKKOS_INC} -O2 -g  -fopenmp -c enabled-omp.cxx
+g++ -o enabled-omp enabled-omp.o -fopenmp -lm -L${TACC_KOKKOS_LIB} -lkokkoscore
+./enabled-omp
+
 program=enabled-gpu
 module load cuda/12 kokkos/4.1.00-cuda
 nvcc -I${TACC_KOKKOS_INC} -O2 -g --extended-lambda -arch=sm_75 -c enabled-gpu.cu
 nvcc -ccbin g++ -o enabled-gpu enabled-gpu.o -lm -L${TACC_KOKKOS_LIB} -lkokkoscore
+./enabled-gpu
