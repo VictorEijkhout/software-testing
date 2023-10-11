63b504352fa96f73d2d6c2ba03ce4d67522f8c88
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-10-11 14:27:51 -0500

    hacked solution to kokkos tests

--- kokkos/glibc_fix.sh
+++ kokkos/glibc_fix.sh
@@ -1,1 +1,2 @@
 export LD_LIBRARY_PATH=$( ~/bin/splitpath LD_LIBRARY_PATH | grep -v "/usr/lib64"  | ~/bin/assemblepath )
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib64
--- kokkos/kokkos_tests.sh
+++ kokkos/kokkos_tests.sh
@@ -2,12 +2,18 @@
 
 module unload cuda
 module load kokkos/4.1.00
+source glibc_fix.sh
+echo ${LD_LIBRARY_PATH}
+
 g++ -I${TACC_KOKKOS_INC} -O2 -g  -fopenmp -c enabled-omp.cxx
 g++ -o enabled-omp enabled-omp.o -fopenmp -lm -L${TACC_KOKKOS_LIB} -lkokkoscore
 ./enabled-omp
 
 program=enabled-gpu
 module load cuda/12 kokkos/4.1.00-cuda
+source glibc_fix.sh
+echo ${LD_LIBRARY_PATH}
+
 nvcc -I${TACC_KOKKOS_INC} -O2 -g --extended-lambda -arch=sm_75 -c enabled-gpu.cu
 nvcc -ccbin g++ -o enabled-gpu enabled-gpu.o -lm -L${TACC_KOKKOS_LIB} -lkokkoscore
 ./enabled-gpu
