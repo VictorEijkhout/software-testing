39d320a90439134597c66d6bd43f3089402a2c7b
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-10-12 12:33:38 -0500

    kokkos tests need c++17

--- kokkos/kokkos_tests.sh
+++ kokkos/kokkos_tests.sh
@@ -5,8 +5,8 @@ module load kokkos/4.1.00
 source glibc_fix.sh
 echo ${LD_LIBRARY_PATH}
 
-g++ -I${TACC_KOKKOS_INC} -O2 -g  -fopenmp -c enabled-omp.cxx
-g++ -o enabled-omp enabled-omp.o -fopenmp -lm -L${TACC_KOKKOS_LIB} -lkokkoscore
+${TACC_CXX} -I${TACC_KOKKOS_INC} -O2 -g  -std=c++17 -fopenmp -c enabled-omp.cxx
+${TACC_CXX} -o enabled-omp enabled-omp.o -fopenmp -lm -L${TACC_KOKKOS_LIB} -lkokkoscore
 ./enabled-omp
 
 program=enabled-gpu
@@ -14,6 +14,6 @@ module load cuda/12 kokkos/4.1.00-cuda
 source glibc_fix.sh
 echo ${LD_LIBRARY_PATH}
 
-nvcc -I${TACC_KOKKOS_INC} -O2 -g --extended-lambda -arch=sm_75 -c enabled-gpu.cu
-nvcc -ccbin g++ -o enabled-gpu enabled-gpu.o -lm -L${TACC_KOKKOS_LIB} -lkokkoscore
-./enabled-gpu
+nvcc -I${TACC_KOKKOS_INC} -O2 -g  -std=c++17 --extended-lambda -arch=sm_75 -c ${program}.cu
+nvcc -ccbin g++ -o ${program} ${program}.o -lm -L${TACC_KOKKOS_LIB} -lkokkoscore
+./${program}
