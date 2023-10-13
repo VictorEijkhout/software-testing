a7c85d41c5830909e225ec2255c385b42da8ec12
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-10-13 16:19:03 -0500

    netcdf sanity test

--- kokkos/kokkos_tests.sh
+++ kokkos/kokkos_tests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 module unload cuda
-module load kokkos/4.1.00
+module load cuda/12 kokkos/4.1.00
 source glibc_fix.sh
 echo ${LD_LIBRARY_PATH}
+
+if [ -z "${TACC_CXX}" ] ; then
+    echo "ERROR set TACC_CXX" && exit 1
+fi
 
 ${TACC_CXX} -I${TACC_KOKKOS_INC} -O2 -g  -std=c++17 -fopenmp -c enabled-omp.cxx
 ${TACC_CXX} -o enabled-omp enabled-omp.o -fopenmp -lm -L${TACC_KOKKOS_LIB} -lkokkoscore
