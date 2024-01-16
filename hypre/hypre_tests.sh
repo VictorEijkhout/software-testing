eea246624cf548dcdfc4d7f428b8156700b8993b
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-01-16 12:16:23 -0600

    hypre cleanup

--- hypre/hypre_tests.sh
+++ hypre/hypre_tests.sh
@@ -4,23 +4,14 @@
 ## run tests, given a loaded compiler
 ##
 
-package=hypre
-if [ -z "${TACC_HYPRE_DIR}" ] ; then 
-    echo "Please load module <<$package>>" && exit 1
+if [ -z "${package}" ] ; then
+    echo "Error: supply package=..." && exit 1 
 fi
-
 if [ -z "${compilelog}" ] ; then
     compilelog=compile.log
-    rm -f ${compilelog}
 fi
 
 source ../failure.sh
 
-set -x
 echo "==== Test if we can compile"
-retcode=0
 ../cmake_test_driver.sh -m -p ${package} -l ${compilelog} has.c
-
-if [ "${compilelog}" = "compile.log" ] ; then
-    echo "See: ${compilelog}"
-fi
