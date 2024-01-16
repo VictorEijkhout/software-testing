94a6931848117535dc05dca841e677316d5e3712
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-01-16 11:03:48 -0600

    petsc test files

---
+++ petsc/F90/fortran.F90
@@ -0,0 +1,5 @@
+Program moduleload
+#include <petsc/finclude/petsc.h>
+  use petsc
+  Mat :: A
+End Program moduleload
---
+++ petsc/c/int.c
@@ -0,0 +1,14 @@
+#include "petsc.h"
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+int main(int argc,char **args)
+{
+  PetscErrorCode ierr;
+
+  PetscInitialize(&argc,&args,0,0);
+  // size of scalar
+  printf("%ld\n",sizeof(PetscInt));
+  return PetscFinalize();
+}
+
