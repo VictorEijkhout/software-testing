1a8f52fc7cb2e1e846589351361bff79b112cfe4
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-01-16 12:16:39 -0600

    mpl still doesn't find cmake

--- mpl/mpl_tests.sh
+++ mpl/mpl_tests.sh
@@ -13,14 +13,5 @@ fi
 
 source ../failure.sh
 
-    # echo && echo "==== Configuration: ${config}"
-    # module load ${compiler} >/dev/null 2>&1
-    # module load ${package}/${version}${extension} >/dev/null 2>&1
-    # if [ $? -eq 0 ] ; then
-
-    # 	case ${compiler} in ( intel* ) fc=ifort ;; ( gcc* ) fc=gfortran ;; esac
-    # 	case ${compiler} in ( intel* ) cc=icc ;; ( gcc* ) cc=gcc ;; esac
-
 echo "==== Test if we can compile CXX with MPL"
-../cmake_test_driver.sh -m -p ${package} -l ${compilelog} has.cpp
-
+../cmake_test_driver.sh -m -p ${package} -l ${compilelog} has.cxx
