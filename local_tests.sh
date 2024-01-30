6145c05e816521e3f3986ecb7eb03872ea8658c8
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-01-30 05:19:41 -0600

    local tests abstracted

---
+++ local_tests.sh
@@ -0,0 +1,24 @@
+module reset >/dev/null 2>&1
+echo "================"
+echo "==== Package: ${package}, version: ${version}"
+echo "==== Local modules"
+echo "================"
+echo 
+compilelog=local_tests.log
+rm -f ${compilelog}
+for compiler in $( cat ../compilers.sh ) ; do
+
+    config=$( echo $compiler | tr -d '/' )
+    echo "==== Configuration: ${config}" | tee -a ${compilelog}
+    source ${HOME}/Software/env_${TACC_SYSTEM}_${config}.sh >/dev/null 2>&1
+    module load ${package}/${version} >/dev/null 2>&1
+    if [ $? -eq 0 ] ; then
+
+	source ${package}_tests.sh
+
+    else
+	echo "WARNING could not load ${package}/${version}"
+    fi
+done
+echo && echo "See: ${compilelog}" && echo
+
