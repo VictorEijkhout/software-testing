91587039f1041dd57b42aef00105b42e11cd504c
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-01-31 12:29:24 -0600

    hdf5 tests reorg

---
+++ netcdf/local_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+package=netcdf
+version=4.9.2
+
+##
+## test all programs for this package,
+## looping over locally available modules
+##
+
+source ../options.sh
+
+source ../local_tests.sh
