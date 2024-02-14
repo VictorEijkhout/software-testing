4c40db93febdb1f13106e8f8642ca1d1e42bc9c5
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-02-14 13:35:29 -0600

    new: trilinos

---
+++ trilinos/local_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+package=hypre
+version=2.30.0
+
+##
+## test all programs for this package,
+## looping over locally available modules
+##
+
+source ../options.sh
+
+source ../local_tests.sh
---
+++ trilinos/tacc_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+package=hypre
+version=2.30.0
+mpi=1
+
+source ../tacc_tests.sh
+
