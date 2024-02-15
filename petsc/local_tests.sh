4c071312992a1045c7567d6e17bfe3a9dea42c44
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-02-15 08:23:09 -0600

    unify

--- petsc/local_tests.sh
+++ petsc/local_tests.sh
@@ -3,43 +3,6 @@
 package=petsc
 version=3.20
 
-extension=
-function usage() {
-    echo "Usage: $0 [ -v version (default=${version} ]"
-}
-if [ $# -eq 1 -a "$1" = "-h" ] ; then
-    usage && exit 0
-fi
-while [ $# -gt 0 ] ; do
-    if [ "$1" = "-x" ] ; then
-	shift && extension="-$1" && shift
-    elif [ "$1" = "-v" ] ; then
-	shift && version="$1" && shift
-    fi
-done
+source ../options.sh
 
-source ../local_tests.sh 
-
-# module reset >/dev/null 2>&1
-# export compilelog=local_test.log
-# rm -f ${compilelog}
-# touch ${compilelog}
-# echo "================"
-# echo "==== Package: ${package}, version: ${version}"
-# echo "==== Local modules"
-# echo "==== logfile: ${compilelog}"
-# echo "================"
-# echo 
-# for compiler in $( cat ../compilers.sh ) ; do
-
-#     config=$( echo $compiler | tr -d '/' )
-#     echo "==== Configuration: ${config}" | tee -a ${compilelog}
-#     retcode=0
-#     source ${HOME}/Software/env_${TACC_SYSTEM}_${config}.sh >/dev/null 2>&1 || retcode=$?
-#     if [ $retcode -eq 0 ] ; then 
-# 	source petsc_tests.sh
-#     else
-# 	echo "==== Could not load configuration ${config}"
-#     fi
-# done
-# echo && echo "See: ${compilelog}" && echo
+source ../local_tests.sh
