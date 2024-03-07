0818f1894f6593cb8fbf2ecce7cbb10462c13ef2
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-03-07 14:00:01 -0600

    b flag

--- petsc/all_local_tests.sh
+++ petsc/all_local_tests.sh
@@ -15,15 +15,18 @@ compiler=
 
 function usage() {
     echo "Usage: $0 [ -v matchedversion (default=${matchversion}) ]"
-    echo "    [ -c compiler ]"
+    echo "    [ -c compiler ] [ -b (skip ibrun) ]"
     if [ ! -z "${extra_help}" ] ; then echo ${extra_help} ; fi
 }
 if [ $# -eq 1 -a "$1" = "-h" ] ; then
     usage && exit 0
 fi
+noibrun=
 while [ $# -gt 0 ] ; do
     if [ "$1" = "-h" ] ; then
 	usage && exit 0
+    elif [ "$1" = "-b" ] ; then
+	noibrun="-b" && shift
     elif [ "$1" = "-c" ] ; then
 	shift && compiler="$1" && shift
     elif [ "$1" = "-v" ] ; then
@@ -37,7 +40,7 @@ for version in ${versions} ; do
     version=${version##${package}/}
     if [[ ${version} = *${matchversion}* ]] ; then 
 	echo "Testing version <<$version>>"
-	./local_tests.sh -v $version \
+	./local_tests.sh -v $version ${noibrun} \
 	    $( if [ ! -z "${compiler}" ] ; then echo "-c $compiler" ; fi )
     else
 	echo "Version skipped: <<$version>>"
