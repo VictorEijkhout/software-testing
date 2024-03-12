407516f152ca64612d19ac92fad823fa5476d3cf
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-03-12 14:58:04 -0500

    skip ibrun

--- petsc/all_tacc_tests.sh
+++ petsc/all_tacc_tests.sh
@@ -15,15 +15,18 @@ compiler=
 
 function usage() {
     echo "Usage: $0 [ -v matchedversion (default=${matchversion}) ]"
-    echo "    [ -c compiler ]"
+    echo "    [ -c compiler ] [ -b (skip ibrun) ]"
     if [ ! -z "${extra_help}" ] ; then echo ${extra_help} ; fi
 }
 if [ $# -eq 1 -a "$1" = "-h" ] ; then
     usage && exit 0
 fi
+noibrun=
 while [ $# -gt 0 ] ; do
     if [ "$1" = "-h" ] ; then
 	usage && exit 0
+    elif [ "$1" = "-b" ] ; then
+	noibrun="-b" && shift
     elif [ "$1" = "-c" ] ; then
 	shift && compiler="$1" && shift
     elif [ "$1" = "-v" ] ; then
@@ -32,6 +35,7 @@ while [ $# -gt 0 ] ; do
 done
 
 
+alllog=all_tacc_tests.log
 versions=$( module -t avail ${package} 2>&1 | sed -e '1d' )
 for version in ${versions} ; do
     version=${version##${package}/}
@@ -42,4 +46,5 @@ for version in ${versions} ; do
     else
 	echo "Version skipped: <<$version>>"
     fi 
-done 2>&1 | tee all_tacc_tests.log
+done 2>&1 | tee ${alllog}
+echo && echo "See: ${alllog}" && echo 
