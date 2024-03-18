da607f89d851b4307168bc692b378d7e44ca15e7
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-03-18 14:52:51 -0500

    pnetcdf tests working

---
+++ parallelnetcdf/local_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+package=parallelnetcdf
+version=1.12.3
+
+##
+## test all programs for this package,
+## looping over locally available modules
+##
+
+source ../options.sh
+
+source ../local_tests.sh
---
+++ parallelnetcdf/tacc_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+package=parallelnetcdf
+version=1.12.3
+
+source ../options.sh
+
+source ../tacc_tests.sh
+
