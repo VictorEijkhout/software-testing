94a3f03ba47fc232eb9708ee11bf75d2655cd159
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-03-29 09:26:57 -0500

    test drivers

---
+++ p4est/local_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+package=p4est
+version=2.8.6
+
+##
+## test all programs for this package,
+## looping over locally available modules
+##
+
+source ../options.sh
+
+source ../local_tests.sh
---
+++ p4est/tacc_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+package=p4est
+version=2.8.6
+mpi=1
+
+source ../options.sh
+source ../tacc_tests.sh
+
