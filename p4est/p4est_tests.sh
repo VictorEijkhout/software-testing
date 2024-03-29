3129f440e9dafb8dec499c7c77050a25c6958588
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-03-29 10:24:27 -0500

    superfluous option

--- p4est/p4est_tests.sh
+++ p4est/p4est_tests.sh
@@ -9,5 +9,4 @@ source ../failure.sh
 
 echo "---- Test if we can compile"
 ../cmake_test_driver.sh -m -p ${package} -l ${logfile} \
-    --cmake "-DHYPRE_INCLUDE_DIRS=${TACC_HYPRE_INC},-DHYPRE_LIBRARY_DIRS=${TACC_HYPRE_LIB},-DHYPRE_LIBRARIES=libHYPRE.so" \
     has.c
