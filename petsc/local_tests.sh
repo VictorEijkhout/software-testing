112cd5987d2edc715d3b9146eebb64cbb4ff5599
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-05-01 09:39:51 -0500

    help string

--- petsc/local_tests.sh
+++ petsc/local_tests.sh
@@ -4,8 +4,7 @@ package=petsc
 version=3.21
 modules=
 mpi=1
+help_string="Loop over all compilers, testing package: ${package}, version: ${version}"
 
-nopython=
-EXTRA_OPTIONS=-4,nopython
 source ../options.sh
 source ../local_tests.sh
--- petsc/tacc_tests.sh
+++ petsc/tacc_tests.sh
@@ -4,7 +4,7 @@ package=petsc
 version=3.21
 modules=
 mpi=1
-help_string="Loop over all compilers, testing one package version"
+help_string="Loop over all compilers, testing package: ${package}, version: ${version}"
 
 source ../options.sh
 source ../tacc_tests.sh
