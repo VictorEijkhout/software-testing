187c04dd8bcfcb695fada803aa34ae968fe900ce
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-05-21 13:55:18 -0500

    adding f09 test

--- mpi/mpi_tests.sh
+++ mpi/mpi_tests.sh
@@ -7,5 +7,8 @@
 source ../test_options.sh
 source ../failure.sh
 
-echo "---- Test if we can compile"
-../cmake_test_driver.sh -m -p ${package} -l ${logfile} mpi.c
+echo "---- Test if we can compile f90"
+../cmake_test_driver.sh -m -p ${package} -l ${logfile} mpif90.c
+
+echo "---- Test if we can compile f08"
+../cmake_test_driver.sh -m -p ${package} -l ${logfile} mpif08.c
