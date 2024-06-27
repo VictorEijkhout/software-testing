2567d025c3fa8f29e6ebff125e13d44afc612647
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-06-27 15:00:07 -0500

    test presence of hdf5

--- python/python_tests.sh
+++ python/python_tests.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+echo && echo "Do we have numpy" && echo
+
 python3 -c "import numpy as np; np.__config__.show()" \
     | grep mkl \
 	   2>/dev/null
 
+echo && echo "Do we have h5py" && echo
+
+python3 -c "import h5py"
