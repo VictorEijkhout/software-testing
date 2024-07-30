9609286f1c00638948115c500c9dfda2f044f477
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-07-30 15:28:24 -0500

    eigen INC test

--- eigen/eigen_tests.sh
+++ eigen/eigen_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##
-## run tests, given a loaded compiler
+## run tests, given a loaded environment
 ##
 
 package=$(pwd) && package=${package##*/}
@@ -11,15 +11,23 @@ source ../failure.sh
 
 retcode=0
 ../cmake_test_driver.sh -p ${package} -l ${logfile} \
-			--title "---- compile and run with pkgconfig" \
+			--title "compile and run with pkgconfig" \
 			-t "package" \
 			has.cxx 
 
 retcode=0
 ../cmake_test_driver.sh -p ${package} -l ${logfile} \
-			--title "---- compile and run with find_package" \
+			--title "compile and run with find_package" \
 			-t "module" \
 			has.cpp
+
+testcaption="contents of INC dir"
+echo "---- Test: ${testcaption}"
+if [ -d "${TACC_EIGEN_INC}/Eigen" ] ; then
+    echo "     has"
+else
+    failure 1 "not finding TACC_EIGEN_INC/Eigen=${TACC_EIGEN_INC}/Eigen"
+fi
 
 if [ "${logfile}" = "compile.log" ] ; then
     echo "See: ${logfile}"
