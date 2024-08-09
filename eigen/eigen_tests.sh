f128679ac0cbcfcea75ae1b34945a1e6104cf804
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-08-09 17:37:14 -0500

    updated existence tests

--- eigen/eigen_tests.sh
+++ eigen/eigen_tests.sh
@@ -9,26 +9,16 @@ package=$(pwd) && package=${package##*/}
 source ../options.sh
 source ../failure.sh
 
-retcode=0
 ../cmake_test_driver.sh -p ${package} -l ${logfile} \
 			--title "compile and run with pkgconfig" \
 			-t "package" \
 			has.cxx 
 
-retcode=0
 ../cmake_test_driver.sh -p ${package} -l ${logfile} \
 			--title "compile and run with find_package" \
 			-t "module" \
 			has.cpp
 
-testcaption="contents of INC dir"
-echo "Test: ${testcaption}"
-if [ -d "${TACC_EIGEN_INC}/Eigen" ] ; then
-    echo "     has"
-else
-    failure 1 "not finding TACC_EIGEN_INC/Eigen=${TACC_EIGEN_INC}/Eigen"
-fi
-
-if [ "${logfile}" = "compile.log" ] ; then
-    echo "See: ${logfile}"
-fi
+../existence_test.sh -p ${package} -l ${logfile} \
+		     --title "core header" \
+		     -d inc Eigen/Core
