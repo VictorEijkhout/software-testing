720681d427ad9b8e033166ff6410a24952c13dff
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-08-15 16:07:24 -0500

    pcre2 tests

---
+++ pcre2/pcre2_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##
+## run tests, given a loaded environment
+##
+
+package=$(pwd) && package=${package##*/}
+
+source ../options.sh
+source ../failure.sh
+
+../existence_test.sh -p ${package} -l ${logfile} \
+		     --title "core header" \
+		     -d lib libpcre2-8.so
+
+../existence_test.sh -p ${package} -l ${logfile} \
+		     --title "core header" \
+		     cmake/pcre2-config.cmake
---
+++ pcre2/tacc_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+package=pcre2
+version=10.43
+
+source ../options.sh
+source ../tacc_tests.sh
