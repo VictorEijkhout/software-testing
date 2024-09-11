cef3d468c24b3e9bff331e8dbb881cc994d69d81
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-09-10 21:52:16 -0500

    adding binrun test

--- proj/proj_tests.sh
+++ proj/proj_tests.sh
@@ -13,3 +13,16 @@ source ../failure.sh
 		     --title "core header" \
 		     -d lib libproj.so
 
+testlog="${logdir}/${source}.log"
+rm -f ${testlog} && touch ${testlog}
+echo "Running bin/proj" | tee -a ${testlog}
+${TACC_PROJ_DIR}/bin/proj >binrun.log 2>&1
+success=$( cat binrun.log | grep usage | wc -l )
+if [ $success -eq 1 ] ; then 
+    failure 0 "able to run proj program" | tee -a ${testlog}
+else
+    failure 1 "failure to run proj program" | tee -a ${testlog}
+fi
+
+cat ${testlog} >> ${logfile}
+
