381b8f049862c9ba3a66d2f70e37fcd21fb68f6d
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-09-17 15:38:30 -0500

    correction for tacc version

--- netcdff/tacc_tests.sh
+++ netcdff/tacc_tests.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+################################################################
+####
+#### Tests for the Fortran interface to Netcdf
+#### In the local install these are separate package, but
+#### in the final TACC modules they are merged
+####
+################################################################
 
 package=netcdff
 version=4.6.1
-modules=netcdf/4.9.2
-loadpackage=${package}
+modules=
+loadpackage=netcdf/4.9.2
 
 source ../options.sh
 source ../tacc_tests.sh
