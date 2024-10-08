11adfdaaac2882d4b88d8377bc3a708908c5cbee
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-10-08 13:52:27 -0500

    petsc 3.22

---
+++ petsc/package.sh
@@ -0,0 +1,10 @@
+##
+## settings common to tacc & local tests
+##
+
+package=petsc
+version=3.22
+modules=
+mkl=1
+mpi=1
+python_option=1
--- petsc/tacc_tests.sh
+++ petsc/tacc_tests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-package=petsc
-version=3.21
-modules=
-mkl=1
-mpi=1
-python_option=1
-help_string="Loop over all compilers, testing package: ${package}, version: ${version}"
+source ./package.sh
 
 source ../options.sh
 source ../tacc_tests.sh
