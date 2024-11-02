2b8c1a76f20e8f4b63c228845239b716e790cc33
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-11-02 13:02:43 -0500

    nf-config test reporting

--- netcdff/netcdff_tests.sh
+++ netcdff/netcdff_tests.sh
@@ -34,9 +34,11 @@ echo "libdir: <<${libdir}>>" >>${logfile}
 if [ ! -d ${libdir} ] ; then
     failure 1 "find nf-config lib dir <<${libdir}>>"
 fi
-nlibs=$( ls ${libdir}/.so 2>/dev/null | wc -l )
+nlibs=$( ls ${libdir}/*.so 2>/dev/null | wc -l )
 if [ $nlibs -eq 0 ] ; then
     failure 1 "find libs in libdir <<${libdir}>>"
+else 
+    failure 0 "find libs in libdir <<${libdir}>>"
 fi
 
 if [ "${logfile}" = "compile.log" ] ; then
