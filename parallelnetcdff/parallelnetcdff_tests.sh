ff97137bf8442f2f634c65676d5f0eba1066565b
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-11-14 08:26:56 -0600

    ignore siesta data

---
+++ parallelnetcdff/parallelnetcdff_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##
+## run tests, given a loaded compiler
+##
+
+source ./package.sh
+command_args=$*
+source ../options.sh
+source ../failure.sh
+set_flags
+
+../cmake_test_driver.sh ${standardflags} \
+			--title "can we compile parallel C" \
+			parallel_vara.c
