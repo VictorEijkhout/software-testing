2ed43079b37167fa7217719c531a7faa0221fe62
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-11-18 13:36:24 -0600

    ldd test

--- mfem/mfem_tests.sh
+++ mfem/mfem_tests.sh
@@ -13,3 +13,10 @@ source ../failure.sh
 			--in-build-run \
 			--title "can we compile and run" \
 			ex0.cpp
+
+echo "TEST ldd"
+unresolved=$( ldd ${TACC_MFEM_LIB}/libmfem.so | grep "not found" | wc -l )
+if [ ${unresolved} -gt 0 ] ; then
+    us=$( u="" && for l in $( ldd ${TACC_MFEM_LIB}/libmfem.so | grep "not found" | awk '{print $1}' ) ; do u="$u $l"; done && echo $u )
+    echo "ERROR unresolved libraries: ${us}"
+fi
