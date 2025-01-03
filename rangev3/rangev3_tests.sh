c3bd3e7f46421387da92e829ef2fc81004a72d5f
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-01-03 09:33:16 -0600

    rangev3 tests

---
+++ rangev3/rangev3_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##
+## run tests, given a loaded compiler
+##
+
+source ./package.sh
+command_args=$*
+source ../options.sh
+source ../failure.sh
+set_flags
+
+##
+## Tests
+##
+../cmake_test_driver.sh ${standardflags} \
+			--title "can we compile and run" \
+			has.cxx 
+
+if [ "${logfile}" = "compile.log" ] ; then
+    echo "See: ${logfile}"
+fi
