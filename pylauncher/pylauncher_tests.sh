63f750badee7a63a18dd874d0252c9268df20a22
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-01-13 14:51:46 -0600

    queue handling

--- pylauncher/pylauncher_tests.sh
+++ pylauncher/pylauncher_tests.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+QUEUE_frontera=small # 2 nodes
+QUEUE_ls6=normal
+QUEUE_stampede3=skx
+QUEUE_vista=gg
 export cores_per_node=$(
     case ${TACC_SYSTEM} in \
 	( vista ) echo 112 ;;
 	( frontera )  echo 56 ;;
 	( ls6 ) echo 128 ;;
-	( stampede ) echo 48 ;;
+	( stampede3 ) echo 48 ;;
     esac )
 echo "Using ${cores_per_node} cores per node"
 
 make clean 
-# classic comma ibrun
-for e in node ; do 
+# 
+for e in classic comma ibrun node ; do 
     echo "Running example: $e"
     make --no-print-directory script submit \
-	NAME=${e} \
-	CORESPERNODE=${cores_per_node} \
-	EXECUTABLE=example_${e}_launcher
+	 NAME=${e} \
+	 QUEUE=$( eval echo \${QUEUE_${TACC_SYSTEM}} ) \
+	 CORESPERNODE=${cores_per_node} \
+	 EXECUTABLE=example_${e}_launcher
 done
 
