9319a77fa8c8b52be8c0a9168885e25d69250748
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-01-15 11:03:18 -0600

    blis tests don't quite work

---
+++ blis/blis_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##
+## run tests, given a loaded compiler
+##
+
+source ./package.sh
+command_args=$*
+source ../options.sh
+source ../failure.sh
+set_flags
+
+cd c
+make clean RM_F="rm -f"
+rm -f *.x *.o *~
+make 00obj_basic.o TEST_BINS=00obj_basic.x \
+    ENABLE_VERBOSE=yes RM_F="rm -f" CC=${TACC_CC} \
+    BLIS_INSTALL_PATH=${TACC_BLIS_DIR} \
+    LIBBLIS_LINK="-L${TACC_BLIS_LIB} -lblis"
+make 00obj_basic.x TEST_BINS=00obj_basic.x \
+    ENABLE_VERBOSE=yes RM_F="rm -f" CC=${TACC_CC} \
+    BLIS_INSTALL_PATH=${TACC_BLIS_DIR} \
+    LIBBLIS_LINK="-L${TACC_BLIS_LIB} -lblis"
---
+++ blis/package.sh
@@ -0,0 +1,2 @@
+package=blis
+version=1.0
