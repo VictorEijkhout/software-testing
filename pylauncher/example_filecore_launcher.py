742eb82bfa39b308477a737a848b33f36da5d3c9
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-03-06 19:34:50 -0600

    file core tester

---
+++ pylauncher/example_filecore_launcher.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+################################################################
+####
+#### This file is part of the `pylauncher' package
+#### for parametric job launching
+####
+#### Copyright Victor Eijkhout 2010-2025
+#### eijkhout@tacc.utexas.edu
+####
+#### https://github.com/TACC/pylauncher
+####
+################################################################
+
+import os
+import pylauncher
+
+##
+## Emulate the classic launcher, using a one liner
+##
+
+example="filecore"
+print( f"Script: {example}_launcher\n .. running: ClassicLauncher" )
+print( " .. input: filecorelines\n" )
+
+pylauncher.ClassicLauncher\
+    ( "filecorelines",cores="file",
+     # optional spec of output dir:
+     workdir=f"pylauncher_tmp_{example}_{ os.environ['SLURM_JOBID'] }",
+     debug="host+exec+job")
