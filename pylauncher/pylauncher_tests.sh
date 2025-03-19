80fecfb7a9dfe35b246d54f72169fbfaace63e47
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-03-19 15:40:44 -0500

    better test organization

--- pylauncher/pylauncher_tests.sh
+++ pylauncher/pylauncher_tests.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-examples="classic comma filecore ibrun node"
+#
+# default examples
+# note: submit needs to come last!
+#
+examples="classic comma core filecore ibrun node gpu submit"
 if [ "$1" = "-h" ] ; then
     echo "Usage: $0 [ -h ] [ -e ex1,ex2,ex3,... ]"
     echo " where examples: ${examples}"
     exit 0
 fi
-
+recompile=1
 while [ $# -gt 0 ] ; do
     if [ "$1" = "-e" ] ; then
-	shift && examples=$1 && shift
+	shift && examples=$1 && recompile=0 && shift
     # elif [ "$1" = "-g" ] ; then 
     # 	gpu=1 && shift
     else
@@ -30,37 +34,44 @@ export cores_per_node=$(
     esac )
 echo "Using ${cores_per_node} cores per node"
 
-make clean 
+if [ $recompile -eq 1 ] ; then
+    make totalclean
+    if [ $? -gt 0 ] ; then echo 1 ; fi
+    make programs
+    if [ $? -gt 0 ] ; then echo 1 ; fi
+fi
+
 # 
+# Example loop
+#
 for e in \
         $( echo ${examples} | tr ',' ' ' ) \
-    ; do 
+	; do
     echo "Running example: $e"
-    make --no-print-directory script submit \
-	NAME=${e} \
-	QUEUE=$( eval echo \${QUEUE_${TACC_SYSTEM}} ) \
-	CORESPERNODE=${cores_per_node} \
-	EXECUTABLE=${e}
+    if [ $e = "submit" ] ; then
+	python3 example_submit_launcher.py 2>&1 | tee -a pylaunchertestsubmit.o000
+    elif [ $e = "gpu" ] ; then
+	case ${TACC_SYSTEM} in \
+	    ( vista )
+            make --no-print-directory script submit \
+		 NAME=gpu EXECUTABLE=gpu
+	    ;;
+	    ( frontera ) 
+            make --no-print-directory script submit \
+		 NAME=gpu EXECUTABLE=gpu
+	    ;;
+	    ( ls6 ) 
+            make --no-print-directory script submit \
+		 NAME=gpu EXECUTABLE=gpu
+	    ;;
+	    ( * ) echo "No GPUs on system ${TACC_SYSTEM} to test" ;;
+	esac
+    else
+	make --no-print-directory script submit \
+	     NAME=${e} \
+	     QUEUE=$( eval echo \${QUEUE_${TACC_SYSTEM}} ) \
+	     CORESPERNODE=${cores_per_node} \
+	     EXECUTABLE=${e}
+    fi
 done
 
-case ${TACC_SYSTEM} in \
-    ( vista )
-        make --no-print-directory script submit \
-	     NAME=gpu EXECUTABLE=gpu
-	;;
-    ( frontera ) 
-        make --no-print-directory script submit \
-	     NAME=gpu EXECUTABLE=gpu
-	;;
-    ( ls6 ) 
-        make --no-print-directory script submit \
-	     NAME=gpu EXECUTABLE=gpu
-	;;
-    ( * ) echo "No GPUs on system ${TACC_SYSTEM} to test" ;;
-esac
-
-##
-## Submit launcher test
-## interactively, probably after everything else is done?
-##
-python3 example_submit_launcher.py 2>&1 | tee -a pylaunchertestsubmit.o000
