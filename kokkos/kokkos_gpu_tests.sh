d0131b1a27b5477cba7200a97667e16f4789d764
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-04-01 10:19:35 -0500

    kokkos cuda update

--- kokkos/kokkos_gpu_tests.sh
+++ kokkos/kokkos_gpu_tests.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+source package.sh
+
 program=enabled-gpu
 retcode=0
-module load cuda/12 kokkos/4.1.00-cuda || retcode=$?
+module load cuda/12 kokkos/4.5.01-cuda || retcode=$?
 if [ $retcode -gt 0 ] ; then
     echo "Could not load kokkos-cuda for this compiler" && exit 10
 fi
-source glibc_fix.sh
-##echo ${LD_LIBRARY_PATH}
+## source glibc_fix.sh
 
 nvcc -I${TACC_KOKKOS_INC} -O2 -g  -std=c++17 --extended-lambda -arch=sm_75 -c ${program}.cu \
      || retcode=$?
