0a202c136cdf71367b3d0b09cbcce99bc7fc2e3a
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-05-12 16:01:25 -0500

    test on src tree example

--- p4est/p4est_tests.sh
+++ p4est/p4est_tests.sh
@@ -7,5 +7,11 @@ source ../test_setup.sh
 ##
 
 ../cmake_test_driver.sh ${standardflags} -l ${logfile} \
-			--title "if we can compile" \
+    --title "if we can compile" \
     has.c
+
+# note: ldd test does not work on cmake driver, only on existence test
+../cmake_test_driver.sh ${standardflags} -l ${logfile} \
+    --ldd \
+    --title "example from src trree" \
+    simple2.c
