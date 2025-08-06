e6b09c0b18e39c4d9b4964a9bd43464aab47aed6
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-08-06 12:26:21 -0500

    adding fortran test

--- hypre/hypre_tests.sh
+++ hypre/hypre_tests.sh
@@ -7,6 +7,11 @@ source ../test_setup.sh
 ##
 
 ../cmake_test_driver.sh ${standardflags} -l ${logfile} \
-			--title "if we can compile" \
+			--title "can we compile C" \
     --cmake "-DHYPRE_INCLUDE_DIRS=${TACC_HYPRE_INC},-DHYPRE_LIBRARY_DIRS=${TACC_HYPRE_LIB},-DHYPRE_LIBRARIES=libHYPRE.so" \
     has.c
+
+../cmake_test_driver.sh ${standardflags} -l ${logfile} \
+			--title "can we compile Fortran" \
+    --cmake "-DHYPRE_INCLUDE_DIRS=${TACC_HYPRE_INC},-DHYPRE_LIBRARY_DIRS=${TACC_HYPRE_LIB},-DHYPRE_LIBRARIES=libHYPRE.so" \
+    ex5f.f
