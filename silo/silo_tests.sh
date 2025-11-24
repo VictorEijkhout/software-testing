38b817b81ee3245e857bcd11537371b5ebd73402
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-11-23 20:40:42 -0600

    silo pthread flag

--- silo/silo_tests.sh
+++ silo/silo_tests.sh
@@ -8,11 +8,11 @@ source ../test_setup.sh
 
 ../cmake_test_driver.sh ${standardflags} -l ${logfile} \
     --title "point functions cxx" \
-    --cmake -DSILO_INC=${TACC_SILO_INC},-DSILO_LIB=${TACC_SILO_LIB} \
+    --cmake -DSILO_INC=${TACC_SILO_INC},-DSILO_LIB=${TACC_SILO_LIB},-DCMAKE_EXE_LINKER_FLAGS=-lpthread \
     point.cxx
 
 ../cmake_test_driver.sh ${standardflags} -l ${logfile} \
     --title "point functions c" \
-    --cmake -DSILO_INC=${TACC_SILO_INC},-DSILO_LIB=${TACC_SILO_LIB} \
+    --cmake -DSILO_INC=${TACC_SILO_INC},-DSILO_LIB=${TACC_SILO_LIB},-DCMAKE_EXE_LINKER_FLAGS=-lpthread \
     point.c
 
