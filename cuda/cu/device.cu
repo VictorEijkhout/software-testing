d354e59b2350bbf2d8d58269833d3f5756a1561b
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-02-06 13:20:55 -0600

    better reporting

--- cuda/cu/device.cu
+++ cuda/cu/device.cu
@@ -10,51 +10,62 @@
  ****************************************************************/
 
 #include <iostream>
-using std::cout;
+using std::cerr;
 
 int main() {
 
   const int ndev = 
     [] () ->int {
-      //codesnippet cudevcount
       int ndev;
-      auto status = cudaGetDeviceCount(&ndev);
-      //codesnippet end
+      cudaError_t status = cudaGetDeviceCount(&ndev);
       if (status==cudaSuccess)
 	return ndev;
       else if (status==cudaErrorNoDevice) {
-	cout << "No devices found\n"; throw status;
+	cerr << "No devices found\n"; throw status;
       } else if (status==cudaErrorInsufficientDriver) {
-	cout << "Insufficient driver\n"; throw status;
-      } else return 0; }();
+	cerr << "Insufficient driver\n"; throw status;
+      } else {
+	cerr << "Unknown cudaError_t: " << status << '\n';
+	throw status;
+      }
+    }();
 
-  cout << "Number of devices detected: " << ndev << '\n';
+  cerr << "Number of devices detected: " << ndev << '\n';
   for ( int idev=0; idev<ndev; ++idev ) {
-    //codesnippet cudevprop
     cudaDeviceProp properties;
-    cudaGetDeviceProperties(&properties,idev);
-    //codesnippet end
-    //codesnippet cudevprops
-    cout << "Device " << idev << "=" << properties.name << '\n';
-    cout << "  async: " << properties.asyncEngineCount << '\n';
-    cout << "  unified: " << properties.unifiedAddressing << '\n';
+    try {
+      cudaGetDeviceProperties(&properties,idev);
+    } catch (...) {
+      cerr << "cudaGetDeviceProperties throws exception" << '\n';
+      throw;
+    }
+    cerr << "Device " << idev << "=" << properties.name << '\n';
+    cerr << "  async: " << properties.asyncEngineCount << '\n';
+    cerr << "  unified: " << properties.unifiedAddressing << '\n';
 
-    cout << "  capability: " << properties.major << "." << properties.minor << '\n';
-    cout << "  multiprocs: " << properties.multiProcessorCount << '\n';
-    //    cout << "  clock rate: " << properties.clockRate << '\n';
+    cerr << "  capability: " << properties.major << "." << properties.minor << '\n';
+    cerr << "  multiprocs: " << properties.multiProcessorCount << '\n';
+    //    cerr << "  clock rate: " << properties.clockRate << '\n';
 
-    cout << "  global memory: " << properties.totalGlobalMem << '\n';
-    cout << "  shared mem/block: " << properties.sharedMemPerBlock << '\n';
-    //codesnippet end
+    cerr << "  global memory: " << properties.totalGlobalMem << '\n';
+    cerr << "  shared mem/block: " << properties.sharedMemPerBlock << '\n';
 
-    cout << "  max threads/block: " << properties.maxThreadsPerBlock << '\n';
-    cout << "  max thread dims: " << properties.maxThreadsDim[0] << ","
+    cerr << "  max threads/block: " << properties.maxThreadsPerBlock << '\n';
+    cerr << "  max thread dims: " << properties.maxThreadsDim[0] << ","
 	 << properties.maxThreadsDim[1] << "," 
 	 << properties.maxThreadsDim[2] << '\n';
   }
 
 #if CUDA_VERSION >= 12000
-  cudaInitDevice(0,0,0);
+  try {
+    cudaError_t status = cudaInitDevice(0,0,0);
+    if (status==cudaSuccess) {
+      cerr << "device successfully initialized" << '\n';
+    } else {
+      cerr << "cudaInitDevice returned " << status << '\n';
+      throw;
+    }
+  } catch (...) {
 #endif
 
   return 0;
