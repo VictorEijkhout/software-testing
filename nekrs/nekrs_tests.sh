fbc37f8d8ead3ffc32d480b2ee1f1391867c130b
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-03-07 11:23:28 -0600

    standard flags

--- nekrs/nekrs_tests.sh
+++ nekrs/nekrs_tests.sh
@@ -8,7 +8,7 @@ source ../test_setup.sh
 
 rm -rf channel/.cache
 export NEKRS_HOME=${TACC_NEKRS_DIR}
-../existence_test.sh -p ${package} -l ${logfile} \
+../existence_test.sh ${standardflags} -l ${logfile} \
     --title "channel example" \
     --ldd \
     --run_in_dir channel --run_args "--setup channel/channel.par" \
