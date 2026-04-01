b131e78379c4ca5291344d4c76aa985f70f2218e
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-04-01 10:35:34 -0500

    pugixml tests

--- pugixml/pugixml_tests.sh
+++ pugixml/pugixml_tests.sh
@@ -6,19 +6,16 @@ source ../test_setup.sh
 ## Tests
 ##
 
-## VLE these are copied from basix : not yet adapted to pugixml
-
 ../existence_test.sh -p ${package} -l ${logfile} \
 		     --title "header" \
-		     --dir inc finite-element.h
+		     --dir inc pugixml.hpp
 
 ../existence_test.sh -p ${package} -l ${logfile} \
-		     --title "cmake config" \
-		     --dir lib cmake/basix/BasixConfig.cmake
+		     --title "library" \
+		     --ldd \
+		     --dir lib libpugixml.so
 
-../existence_test.sh \
-    -p ${package} -l ${logfile} \
-    --title "shared library" \
-    --ldd -r \
-    --dir lib libbasix.so
+../existence_test.sh -p ${package} -l ${logfile} \
+		     --title "pkg config" \
+		     --dir lib pkgconfig/pugixml.pc
 
